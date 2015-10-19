2097cbe32619ab3f03a71c02e67cd622e4c73c27
Phuong Dinh <dinhp@carleton.edu>
2015-10-19 13:00:30 -0500

    oepn file

--- HW6-POSTagging/hmm_model_build.py
+++ HW6-POSTagging/hmm_model_build.py
@@ -1,2 +1,28 @@
 #hmm_model_build.py
 #By Josie and Phuong
+import random
+
+
+def extractSets(filename):
+    #Read data
+    trainingSet = []
+    testSet = []
+    with open(filename, "r") as f:
+        lines = f.read().splitlines()
+        for i in range(len(lines)-1):
+            r = random.random()
+            if r < 0.1:
+                testSet.append(lines[i])
+            else:
+                trainingSet.append(lines[i])
+
+    return trainingSet, testSet
+
+def main():
+    trainingSet, testSet = extractSets("brown_tagged.dat")
+    print(len(testSet))
+
+
+
+
+main()
