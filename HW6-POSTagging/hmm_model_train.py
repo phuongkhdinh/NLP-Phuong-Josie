af516de7f64955c0cd1d8a5e2dc1c3869c23b869
Phuong Dinh <dinhp@carleton.edu>
2015-10-22 09:41:35 -0500

    change forward

--- HW6-POSTagging/hmm_model_train.py
+++ HW6-POSTagging/hmm_model_train.py
@@ -3,45 +3,70 @@
 from collections import Counter
 import sys
 import pickle
+from math import log, exp
 
 # A = tag1, tag2 : prob
 # B = word, tag : prob
 # states = dict of states
 
+_NINF = float('-inf')
+def log_add(values):
+    """
+    Unlog values, Adds the values, returning the log of the addition.
+    """
+    x = max(values)
+    if x > _NINF:
+        sum_diffs = 0
+        for value in values:
+            sum_diffs += exp(value - x)
+        return x + log(sum_diffs)
+    else:
+        return x
+
+
 def forward(A, B, states, obs):
     T = len(obs)
     N = len(states)
     alpha = [{} for i in range(T)]
+
     for s in states:
 
         #TODO: FIX WITH UNK????
-        aVal = 0 if ("START", s) not in A else A[("START", s)]
-        bVal = 0 if (obs[0], s) not in B else B[(obs[0], s)] # if Run on corpus, need to handle Unknown here
-
-        alpha[0][s] = aVal*bVal
+        #aVal = 0.0001 if ("START", s) not in A else A[("START", s)]
+        #bVal = 0.0001 if (obs[0], s) not in B else B[(obs[0], s)] # if Run on corpus, need to handle Unknown here
+        aVal = A[('START', s)]
+        bVal = B[(obs[0], s)]
+        if aVal!= 0 and bVal != 0:
+            alpha[0][s] = aVal + aVal
+        #print(A)
+            print(s, alpha[0][s])
     #print("______")
     #print(A)
     #print(alpha[0])
     for t in range(1, T):
         for s in states:
-            sum = 0
+            sum = _NINF
             for sPrime in states:
-                aVal = 0 if (sPrime,s) not in A else A[(sPrime,s)]
-                bVal = 0 if (obs[t], s) not in B else B[(obs[t], s)]
-                sum += alpha[t-1][sPrime]*aVal*bVal
+                #aVal = 0.0001 if (sPrime,s) not in A else A[(sPrime,s)]
+                #bVal = 0.0001 if (obs[t], s) not in B else B[(obs[t], s)]
+                if sPrime in alpha[t-1]:
+                    aVal = A[(sPrime,s)]
+                    sum = log_add([sum, alpha[t-1][sPrime]+aVal])
                 #print(alpha[t-1][sPrime], aVal, bVal)
                 # if bVal == 0:
                 #     print(obs[t], s)
+            bVal = B[(obs[t], s)]
+            alpha[t][s] = sum + bVal
 
-            alpha[t][s] = sum
-
-    sum = 0
+    sum = _NINF
     for s in states:
-        aVal = 0 if (s, "END") not in A else A[(s, "END")]
-        sum += alpha[T-1][s]*aVal
+        if s in alpha[T-1]:
+            aVal = A[(s, "END")]
+            sum = log_add([sum, alpha[T-1][s]+aVal])
     alpha[T-1]["END"] = sum
     # if alpha[T-1]["END"] == 0:
     #     print(alpha[T-1])
+    print(alpha)
     return alpha
 
 
@@ -79,6 +104,7 @@ def converged(i):
 # O = Obs, V = Output vocab, Q = Hidden states (POS), A = transition, B = emission
 def forwardBackward(Obs, V, Q, A, B):
     alpha = forward(A, B, Q, Obs)
+    print(alpha)
     beta = backward(A, B, Q, Obs)
     T = len(Obs)
     N = len(Q)
@@ -96,6 +122,7 @@ def forwardBackward(Obs, V, Q, A, B):
         for t in range(T-1):
             for j in Q:
                 gamma[t][j] = (alpha[t][j]*beta[t][j])/alpha[-1]["END"]
+                #print(gamma[t][j])
                 for i in Q:
                     # print((alpha[t][i]))
                     # print(aHat[(i,j)])
@@ -103,16 +130,18 @@ def forwardBackward(Obs, V, Q, A, B):
                     # print(beta[t+1][j])
                     # print(alpha[T-1]["END"])
                     zeta[t][(i,j)] = (alpha[t][i]*aHat[(i,j)]*bHat[(Obs[t+1],j)]*beta[t+1][j])/alpha[T-1]["END"]
+                    #print(zeta[t][(i,j)])
         #M-Step
         #ahat i j
         for i in Q:
             for j in Q:
                 numerator = 0
-                denomenator = 0.000000000000000000000000000000000001
+                denomenator = 0
                 for t in range(T-1):
                     numerator += zeta[t][(i,j)]
                     minidenom = 0
                     for k in Q:
+                        #print(zeta[t][(i,k)])
                         minidenom += zeta[t][(i,k)]
                     denomenator += minidenom
                 aHat[(i,j)] = numerator/denomenator
@@ -136,11 +165,11 @@ def forwardBackward(Obs, V, Q, A, B):
 
 def main():
     #modelFile = sys.argv[1]
-    modelFile = "countmodel_FWBW"
+    modelFile = "countmodel"
     # for line in sys.stdin:
     #     obs = line.split(" ")
 
-    with open('countmodel.dat', 'rb') as handle:
+    with open('countmodel_FWBW.dat', 'rb') as handle:
         matrixes = pickle.loads(handle.read())
     A = Counter(matrixes["aMatrix"])
     B = Counter(matrixes["bMatrix"])
