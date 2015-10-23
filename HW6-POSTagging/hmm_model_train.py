8420b887db5ade2db5400d85b2118f668394f375
Josie Bealle <beallej@carleton.edu>
2015-10-22 22:50:12 -0500

    still broken

--- HW6-POSTagging/hmm_model_train.py
+++ HW6-POSTagging/hmm_model_train.py
@@ -4,6 +4,7 @@ from collections import Counter
 import sys
 import pickle
 from math import log, exp
+import time
 
 # A = tag1, tag2 : prob
 # B = word, tag : prob
@@ -33,77 +34,99 @@ def forward(A, B, states, obs):
         firstWord = obs[0]
         #if (firstWord,s) not in B: #We have the first word with this tag
         #    firstWord = "<UNK>"
-        #TODO: FIX WITH UNK????
-        #aVal = 0.0001 if ("START", s) not in A else A[("START", s)]
+      #aVal = 0.0001 if ("START", s) not in A else A[("START", s)]
         #bVal = 0.0001 if (obs[0], s) not in B else B[(obs[0], s)] # if Run on corpus, need to handle Unknown here
         aVal = A[('START', s)]
         bVal = B[(firstWord, s)]
-        if aVal!= 0 and bVal != 0:
-            alpha[0][s] = aVal + bVal
+
+        alpha[0][s] = aVal*bVal
+
+                            #
+                            # if aVal!= 0 and bVal != 0:
+                            #     alpha[0][s] = aVal + bVal
 
     for t in range(1, T):
         for s in states:
-            sum = _NEG_INF
+            sum = 0
             for sPrime in states:
                 #aVal = 0.0001 if (sPrime,s) not in A else A[(sPrime,s)]
                 #bVal = 0.0001 if (obs[t], s) not in B else B[(obs[t], s)]
-                if sPrime in alpha[t-1]:
-                    aVal = A[(sPrime,s)]
-                    sum = log_add([sum, alpha[t-1][sPrime]+aVal])
-                #print(alpha[t-1][sPrime], aVal, bVal)
-                # if bVal == 0:
-                #     print(obs[t], s)
-            word = obs[t]
-            #if (word, s) not in B:
-            #    word = "<UNK>"
-            bVal = B[(word, s)]
-            alpha[t][s] = sum + bVal
+                aVal = A[(sPrime, s)]
+                bVal = B[(obs[t], s)]
+                sum += alpha[t-1][sPrime]*aVal*bVal
+                            # if sPrime in alpha[t-1]:
+                            #     aVal = A[(sPrime,s)]
+                            #     sum += alpha[t-1][sPrime]*aVal
+                            #     sum = log_add([sum, alpha[t-1][sPrime]+aVal])
+                            # #print(alpha[t-1][sPrime], aVal, bVal)
+                            # # if bVal == 0:
+                            # #     print(obs[t], s)
+                            # word = obs[t]
+                            # #if (word, s) not in B:
+                            # #    word = "<UNK>"
+                            # bVal = B[(word, s)]
+                            # alpha[t][s] = sum + bVal
+            alpha[t][s] = sum
+            print(sum)
 
-    sum = _NEG_INF
+    sum = 0
     for s in states:
-        if s in alpha[T-1]:
-            aVal = A[(s, "END")]
-            sum = log_add([sum, alpha[T-1][s]+aVal])
+        aVal = A[(s, "END")]
+        sum += alpha[T-2][s]*aVal
     alpha[T-1]["END"] = sum
-    # if alpha[T-1]["END"] == 0:
-    #     print(alpha[T-1])
+
+
+                            #     if s in alpha[T-1]:
+                            #         aVal = A[(s, "END")]
+                            #         sum = log_add([sum, alpha[T-1][s]+aVal])
+                            # alpha[T-1]["END"] = sum
+    print(alpha[T-1]["END"])
     return alpha
 
 
 def backward(A, B, states, obs):
     N = len(states)
     T = len(obs)
-    beta = [{} for i in range(T)]
+    beta = [{} for i in range(T+2)]
     for i in states:
         if A[(i, "END")] != 0:
-            beta[T-1][i] = A[(i, "END")]
-    for t in range(T-2, -1, -1):
+            beta[T+1][i] = A[(i, "END")]
+    for t in range(T, 0, -1):
         for i in states:
-            sum = _NEG_INF
+            sum = 0
             for j in states:
-                if j in beta[t+1]:
-                    aVal = A[(i,j)]
-                    wd = obs[t+1]
-                    #if (wd, j) not in B:
-                    #    wd = "<UNK>"
-                    bVal = B[(wd, j)]
-                    sum = log_add([sum, beta[t+1][j]+aVal+bVal])
+                aVal = A[(i,j)]
+                bVal = B[(obs[t+1], j)]
+                sum += beta[t+1][j]*aVal*bVal
+
+
+                            # if j in beta[t+1]:
+                            #     aVal = A[(i,j)]
+                            #     wd = obs[t+1]
+                            #     #if (wd, j) not in B:
+                            #     #    wd = "<UNK>"
+                            #     bVal = B[(wd, j)]
+                            #     sum = log_add([sum, beta[t+1][j]+aVal+bVal])
 
 
             beta[t][i] = sum
-    sum = _NEG_INF
+    sum = 0
     for j in states:
-        if j in beta[0]:
-            aVal = A[("START",j)]
-            wd = obs[0]
-            #if (obs[0], j) not in B:
-            #    wd = "<UNK>"
-            bVal = B[(wd, j)]
-            sum = log_add([sum, aVal+bVal+beta[0][j]])
+        aVal = A[("START",j)]
+        bVal = B[(obs[0], j)]
 
+        sum += aVal*bVal*beta[1][j]
     beta[0]["START"] = sum
+                    #if j in beta[0]:
+                    #         aVal = A[("START",j)]
+                    #         wd = obs[0]
+                    #         #if (obs[0], j) not in B:
+                    #         #    wd = "<UNK>"
+                    #         bVal = B[(wd, j)]
+                    #         sum = log_add([sum, aVal+bVal+beta[0][j]])
+                    #
+                    # beta[0]["START"] = sum
     return beta
-
 
 
 
@@ -139,7 +162,7 @@ def forwardBackward(AllObs, V, Q, Amat, Bmat):
         aHat = A
         bHat = B
         ObsCount = 0
-        for Obs in AllObs:
+        for Obs in AllObs[:2]:
             ObsCount += 1
             if ObsCount%50==0:
                 print(ObsCount)
@@ -153,74 +176,90 @@ def forwardBackward(AllObs, V, Q, Amat, Bmat):
 
             #E-Step
             for t in range(T-1):
+                print("meow")
                 for j in Q:
-                    if j in alpha[t] and j in beta[t]:
-                        #print(beta[t][j])
-                        gamma[t][j] = alpha[t][j] + beta[t][j] - alpha[-1]["END"]
-                        #print(gamma[t][j])
-                        for i in Q:
-                            if i in alpha[t] and j in beta[t+1]:
-                                # print((alpha[t][i]))
-                                # print(aHat[(i,j)])
-                                # print(bHat[(Obs[t+1],j)])
-                                # print(beta[t+1][j])
-                                # print(alpha[T-1]["END"])
-                                wd = Obs[t+1]
-
-                                #if (wd, j) not in bHat:
-                                #    wd = "<UNK>"
-                                zeta[t][(i,j)] = alpha[t][i] + aHat[(i,j)] + bHat[(wd,j)] + beta[t+1][j] - alpha[T-1]["END"]
-                            #print(zeta[t][(i,j)])
+                    gamma[t][j] = (alpha[t][j]*beta[t][j])/alpha[-1]["END"]
+                    for i in Q:
+                        zeta[t][(i,j)] = (alpha[t][i]*aHat[(i,j)]*bHat[(Obs[t+1],j)]*beta[t+1][j])/alpha[T-1]["END"]
+                            # if j in alpha[t] and j in beta[t]:
+                            #     #print(beta[t][j])
+                            #     gamma[t][j] = alpha[t][j] + beta[t][j] - alpha[-1]["END"]
+                            #     #print(gamma[t][j])
+                            #     for i in Q:
+                            #         if i in alpha[t] and j in beta[t+1]:
+                            #             # print((alpha[t][i]))
+                            #             # print(aHat[(i,j)])
+                            #             # print(bHat[(Obs[t+1],j)])
+                            #             # print(beta[t+1][j])
+                            #             # print(alpha[T-1]["END"])
+                            #             wd = Obs[t+1]
+                            #
+                            #             #if (wd, j) not in bHat:
+                            #             #    wd = "<UNK>"
+                            #             zeta[t][(i,j)] = alpha[t][i] + aHat[(i,j)] + bHat[(wd,j)] + beta[t+1][j] - alpha[T-1]["END"]
+                            #         #print(zeta[t][(i,j)])
             #M-Step
             #ahat i j
             for i in Q:
                 for j in Q:
-                    numerator = _NEG_INF
-                    denomenator = _NEG_INF
+                    numerator = 0
+                    denomenator = 0
                     for t in range(T-1):
-                        if (i,j) in zeta[t]:
-                            numerator = log_add([numerator, zeta[t][(i,j)]])
-                            minidenom = _NEG_INF
-                            for k in Q:
-                                #print(zeta[t][(i,k)])
-                                if (i,k) in zeta[t]:
-                                    minidenom = log_add([minidenom, zeta[t][(i,k)]])
-                            denomenator = log_add([denomenator, minidenom])
-                    aHat[(i,j)] = numerator-denomenator
+                        numerator += zeta[t][(i,j)]
+                        minidenom = 0
+                        for k in Q:
+                            minidenom += zeta[t][(i,k)]
+                        denomenator += minidenom
+                    aHat[(i,j)] = numerator/denomenator
+                                    #     if (i,j) in zeta[t]:
+                                    #         numerator = log_add([numerator, zeta[t][(i,j)]])
+                                    #         minidenom = _NEG_INF
+                                    #         for k in Q:
+                                    #             #print(zeta[t][(i,k)])
+                                    #             if (i,k) in zeta[t]:
+                                    #                 minidenom = log_add([minidenom, zeta[t][(i,k)]])
+                                    #         denomenator = log_add([denomenator, minidenom])
+                                    # aHat[(i,j)] = numerator-denomenator
 
             #bhat
             for j in Q:
                 for vK in Obs:
-                    numerator = _NEG_INF
-                    denomenator = _NEG_INF
+                    numerator = 0
+                    denomenator = 0
                     for t in range(T):
-                        if j in gamma[t]:
-                            if Obs[t] == vK:
-                                numerator = log_add([numerator, gamma[t][j]])
-                            #print(denomenator, gamma[t][i])
-                            denomenator = log_add([denomenator, gamma[t][j]])
-                    bHat[(vK, j)] = numerator - denomenator
+                        if Obs[t] == vK:
+                            numerator += gamma[t][j]
+                        denomenator += gamma[t][j]
+                    bHat[(vK, j)] = numerator/denomenator
+                                    #     if j in gamma[t]:
+                                    #         if Obs[t] == vK:
+                                    #             numerator = log_add([numerator, gamma[t][j]])
+                                    #         #print(denomenator, gamma[t][i])
+                                    #         denomenator = log_add([denomenator, gamma[t][j]])
+                                    # bHat[(vK, j)] = numerator - denomenator
+
+        start = time.time()
 
         #normalize A
         for i in Q:
             #get sum of state 1
-            tot = []
+            tot = 0
             for j in  Q:
-                tot.append(aHat[(i,j)])
-            tot = log_add(tot)
+                tot += aHat[(i,j)]
             for j in Q:
-                A[(i,j)] = aHat[(i,j)] - tot
+                A[(i,j)] = aHat[(i,j)]/tot
 
         #normalize B
         for i in Q:
             #get sum of state
             tot = []
             for j in V:
-                tot.append(bHat[(j, i)])
-            tot = log_add(tot)
+                tot += bHat[(j, i)]
             for j in V:
-                B[(j, i)] = bHat[(j, i)]- tot
-
+                B[(j, i)] = bHat[(j, i)]/tot
+        end = time.time()
+        print("time: ")
+        print(end - start)
 
 
 
@@ -246,16 +285,23 @@ def main():
     with open('countmodel.dat', 'rb') as handle:
         matrixes = pickle.loads(handle.read())
     states = matrixes["states"]
-    vocab = list(matrixes["vocab"].keys())
+    wds = matrixes["vocab"]
+    if "<s>" in wds:
+        del wds["<s>"]
+    if "</s>" in wds:
+        del wds["</s>"]
+    vocab = list(wds.keys())
     dataSet = matrixes["allData"]
     if "START" in states:
         del states["START"]
     if "END" in states:
         del states["END"]
+
     states = list(states.keys())
     print(states)
-    bProb = log(1/(len(vocab)))
-    aProb = log(1/(len(states)+1))
+    bProb = 1/(len(vocab))
+    print(bProb)
+    aProb = 1/(len(states)+1)
     A = Counter({})
     B = Counter({})
     for state in states:
