e398ac8e810436f474aaf8cbf84cb144bb1f91cf
Josie Bealle <beallej@carleton.edu>
2015-10-24 20:03:21 -0500

    about to try logs

--- HW6-POSTagging/hmm_model_train.py
+++ HW6-POSTagging/hmm_model_train.py
@@ -10,65 +10,70 @@ from decimal import *
 # B = word, tag : prob
 # states = dict of states
 
+def checkUnderflow(pieces, otherinfo):
+    s = 1
+    for piece in pieces:
+        if piece == 0:
+            print("piece is zero ", pieces)
+            if otherinfo != None:
+                print(otherinfo)
+            sys.exit()
+        s *= piece
+    if s== 0:
+        print("prod is zero ", pieces)
+        if otherinfo != None:
+            print(otherinfo)
+        sys.exit()
 
-
-def forward(A, B, states, obs):
-    T = len(obs)
+def forward(A, B, Pi, states, obs):
+    T = len(obs) -1
     N = len(states)
-    alpha = [{} for i in range(T)]
+    alpha = [{} for i in range(T+1)]
     for s in states:
-        firstWord = obs[0]
+        firstWord = obs[1]
 
-        aVal = A[('START', s)]
         bVal = B[(firstWord, s)]
-
-        alpha[0][s] = aVal*bVal
-
+        alpha[1][s] = Pi[s]*bVal
+    #print(Pi)
     for t in range(1, T):
-        for s in states:
-            sum = 0
-            all0 = True
-            a0 = True
-            b= True
-            al0= True
-            for sPrime in states:
-                aVal = A[(sPrime, s)]
-                bVal = B[(obs[t], s)]
-                sum += alpha[t-1][sPrime]*aVal*bVal
+        for j in states:
+            summa = 0
+            bVal = B[(obs[t+1], j)]
+            for i in states:
+                aVal = A[(i,j)]
+                summa += alpha[t][i]*aVal
 
-            alpha[t][s] = sum
+            alpha[t+1][j] = summa*bVal
 
-
+    #print(alpha)
     sum = 0
-    for s in states:
-        aVal = A[(s, "END")]
-        sum += alpha[T-1][s]*aVal
+    for j in states:
+        sum += alpha[T][j]
 
     return alpha, sum
 
 
-def backward(A, B, states, obs):
+def backward(A, B, Pi, states, obs):
     N = len(states)
-    T = len(obs)
-    beta = [{} for i in range(T)]
+    T = len(obs) -1
+    beta = [{} for i in range(T+1)]
     for i in states:
 
-        beta[T-1][i] = A[(i, "END")]
-    for t in range(T-2, -1, -1):
+        beta[T][i] = 1
+    for t in range(T-1, 0, -1):
         for i in states:
             sum = 0
             for j in states:
                 aVal = A[(i,j)]
-                bVal = B[(obs[t-1], j)]
+                bVal = B[(obs[t+1], j)]
                 sum += beta[t+1][j]*aVal*bVal
 
             beta[t][i] = sum
     sum = 0
     for j in states:
-        aVal = A[("START",j)]
-        bVal = B[(obs[0], j)]
-
-        sum += aVal*bVal*beta[0][j]
+        aVal = Pi[j]
+        bVal = B[(obs[1], j)]
+        sum += aVal*bVal*beta[1][j]
 
 
     return beta, sum
@@ -92,12 +97,14 @@ def notConverged(i):
 
 
 # O = Obs, V = Output vocab, Q = Hidden states (POS), A = transition, B = emission
-def forwardBackward(AllObs, V, Q, Amat, Bmat):
+def forwardBackward(AllObs, V, Q, Amat, Bmat, Pimat):
     A = Amat
     B = Bmat
+    Pi = Pimat
 
 
     count = 0
+
     while notConverged(count):
         count += 100
 
@@ -106,27 +113,67 @@ def forwardBackward(AllObs, V, Q, Amat, Bmat):
         aHat = Counter()
         bHat = Counter()
         ObsCount = 0
-        for Obs in AllObs[4:int(len(AllObs)/10)]:
+        obsProbSum = 0
+        for Obs in AllObs[int(len(AllObs)/10):int(len(AllObs)/10)+10]:
+            Obs.insert(0,"")
             ObsCount += 1
             if ObsCount%50==0:
                 print(ObsCount)
-            alpha, alphaSum = forward(A, B, Q, Obs)
-            beta, betaSum = backward(A, B, Q, Obs)
-            print(alphaSum, betaSum)
-            sys.exit()
-            T = len(Obs)
-            N = len(Q)
 
-            gamma = [{} for i in range(T)]
-            zeta = [{} for i in range(T)]
+
+            alpha, alphaSum = forward(A, B,Pi, Q, Obs)
+            beta, betaSum = backward(A, B, Pi, Q, Obs)
+
+
+            if alphaSum != betaSum:
+                print(count, alphaSum, betaSum)
+                #sys.exit()
+
+
+            T = len(Obs)-1
+            N = len(Q)
+            if alphaSum == 0:
+                obsProbSum = float("inf")
+            else:
+                obsProbSum += 1/alphaSum
+            # #eq 1
+            # for i in Q:
+            #     for j in Q:
+            #         summa = 0
+            #         for t in range(1,T):
+            #             alpha[t][i]
+            #             A[(i,j)]
+            #             B[(Obs[t], i)]
+            #             beta[t+1]
+            #             summa+= alpha[t][i]*A[(i,j)]*B[(Obs[t+1], i)]*beta[t+1][i]
+            #         aHat[(i,j)] += summa
+            #
+            # #eq 2
+            # summa = 0
+            # for i in Q:
+            #     for sigma in Obs:
+            #         summa = 0
+            #         for t in range(1,T+1):
+            #             if Obs[t] == sigma:
+            #                 summa += alpha[t][i]*beta[t][i]
+            #         bHat[(sigma, i)] += summa
+
+
+
+
+
+
+            gamma = [{} for i in range(T+1)]
+            zeta = [{} for i in range(T+1)]
 
             #E-Step
-            for t in range(T):
+            for t in range(1,T+1):
                 for j in Q:
-
+                    #checkUnderflow([alpha[t][j],beta[t][j]], 50)
                     gamma[t][j] = (alpha[t][j]*beta[t][j])/alphaSum
                     for i in Q:
-                        if t < T-1:
+                        if t < T:
+                            #checkUnderflow([alpha[t][i],A[(i,j)],B[(Obs[t+1],j)],beta[t+1][j]], 50)
                             zeta[t][(i,j)] = (alpha[t][i]*A[(i,j)]*B[(Obs[t+1],j)]*beta[t+1][j])/alphaSum
 
             #M-Step
@@ -134,7 +181,7 @@ def forwardBackward(AllObs, V, Q, Amat, Bmat):
                 for j in Q:
                     numerator = 0
                     denomenator = 0
-                    for t in range(T-1):
+                    for t in range(1,T):
                         numerator += zeta[t][(i,j)]
                         minidenom = 0
                         for k in Q:
@@ -148,33 +195,52 @@ def forwardBackward(AllObs, V, Q, Amat, Bmat):
                 for vK in Obs:
                     numerator = 0
                     denomenator = 0
-                    for t in range(T):
+                    for t in range(1,T+1):
                         if Obs[t] == vK:
                             numerator += gamma[t][j]
                         denomenator += gamma[t][j]
                     bHat[(vK, j)] += numerator/denomenator
 
+        # print("________________")
+        # #print(A)
+        # Ast = A.copy()
+        # #normalize A
 
-        #normalize A
+
+        # #make up for eq1, 2
+        # for i in Q:
+        #     for j in Q:
+        #         aHat[(i,j)] = aHat[(i,j)] * obsProbSum
+        #     for sigma in V:
+        #         bHat[(sigma, i)] = bHat[(sigma, i)] * obsProbSum
+
+
+        # totstart = 0
+        # totend = 0
+        #
         for i in Q:
             #get sum of state 1
             tot = 0
             for j in  Q:
                 tot += aHat[(i,j)]
             for j in Q:
-                A[(i,j)] = aHat[(i,j)]/tot
+                if tot != 0:
+                    A[(i,j)] = aHat[(i,j)]/tot
 
-        print(B)
+
         #normalize B
         for i in Q:
             #get sum of state
             tot = 0
+            zeroB = 0
             for j in V:
                 tot += bHat[(j, i)]
             for j in V:
-                B[(j, i)] = bHat[(j, i)]/tot
-                print(B[(j, i)])
+                if tot != 0:
+                 B[(j, i)] = bHat[(j, i)]/tot
 
+
+  # print(A, B)
     return A, B
 
 
@@ -217,10 +283,11 @@ def main():
     states = list(states.keys())
     bProb = 1/(len(vocab))
     aProb = 1/(len(states)+1)
-    A = Counter({})
-    B = Counter({})
+    A = Counter()
+    B = Counter()
+    Pi = Counter()
     for state in states:
-        A[("START", state)] = aProb
+        Pi[state] = aProb
 
     for state1 in states:
         for state2 in states:
@@ -232,12 +299,13 @@ def main():
     for word in vocab:
         for state in states:
             B[(word,state)] = bProb
+    print(B)
 
     V = vocab
     Q = states
 
 
-    aHat, bHat = forwardBackward(dataSet, V, Q, A, B)
+    aHat, bHat = forwardBackward(dataSet, V, Q, A, B, Pi)
     hmmCountModel = {}
         
     with open("trainmodel.dat", "wb") as outFile:
