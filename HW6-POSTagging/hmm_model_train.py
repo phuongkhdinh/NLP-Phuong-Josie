e2fa90158eb27c558b8f96f97a98702faf93d395
Josie Bealle <beallej@carleton.edu>
2015-10-25 15:13:36 -0500

    cleanup

--- HW6-POSTagging/hmm_model_train.py
+++ HW6-POSTagging/hmm_model_train.py
@@ -26,7 +26,7 @@ def checkUnderflow(pieces, otherinfo):
         sys.exit()
 
 def forward(A, B, Pi, states, obs):
-    T = len(obs) -1
+    T = len(obs) - 1
     N = len(states)
     alpha = [{} for i in range(T+1)]
     for s in states:
@@ -56,7 +56,6 @@ def backward(A, B, Pi, states, obs):
     T = len(obs) -1
     beta = [{} for i in range(T+1)]
     for i in states:
-
         beta[T][i] = 1
     for t in range(T-1, 0, -1):
         for i in states:
@@ -65,7 +64,6 @@ def backward(A, B, Pi, states, obs):
                 aVal = A[(i,j)]
                 bVal = B[(obs[t+1], j)]
                 sum += beta[t+1][j]*aVal*bVal
-
             beta[t][i] = sum
     sum = 0
     for j in states:
@@ -103,6 +101,8 @@ def forwardBackward(AllObs, V, Q, Amat, Bmat, Pimat):
 
 
     count = 0
+
+    #insert empty obs for indexing purposes
     for Obs in AllObs:
         Obs.insert(0, "")
     while notConverged(count):
@@ -115,53 +115,15 @@ def forwardBackward(AllObs, V, Q, Amat, Bmat, Pimat):
         bHat = Counter()
         piHat = Counter()
         ObsCount = 0
-        obsProbSum = 0
         for Obs in AllObs:
-        #for Obs in AllObs[:10]:
             ObsCount += 1
-
-
 
             alpha, alphaSum = forward(A, B,Pi, Q, Obs)
             beta, betaSum = backward(A, B, Pi, Q, Obs)
 
 
-            # if alphaSum != betaSum:
-            #     print(count, alphaSum, betaSum)
-                #sys.exit()
-
-
             T = len(Obs)-1
             N = len(Q)
-            if alphaSum == 0:
-                obsProbSum = float("inf")
-            else:
-                obsProbSum += 1/alphaSum
-            # #eq 1
-            # for i in Q:
-            #     for j in Q:
-            #         summa = 0
-            #         for t in range(1,T):
-            #             alpha[t][i]
-            #             A[(i,j)]
-            #             B[(Obs[t], i)]
-            #             beta[t+1]
-            #             summa+= alpha[t][i]*A[(i,j)]*B[(Obs[t+1], i)]*beta[t+1][i]
-            #         aHat[(i,j)] += summa
-            #
-            # #eq 2
-            # summa = 0
-            # for i in Q:
-            #     for sigma in Obs:
-            #         summa = 0
-            #         for t in range(1,T+1):
-            #             if Obs[t] == sigma:
-            #                 summa += alpha[t][i]*beta[t][i]
-            #         bHat[(sigma, i)] += summa
-
-
-
-
 
 
             gamma = [{} for i in range(T+1)]
@@ -171,11 +133,9 @@ def forwardBackward(AllObs, V, Q, Amat, Bmat, Pimat):
             #E-Step
             for t in range(1,T+1):
                 for j in Q:
-                    #checkUnderflow([alpha[t][j],beta[t][j]], 50)
                     gamma[t][j] = (alpha[t][j]*beta[t][j])/alphaSum
                     for i in Q:
                         if t < T:
-                            #checkUnderflow([alpha[t][i],A[(i,j)],B[(Obs[t+1],j)],beta[t+1][j]], 50)
                             zeta[t][(i,j)] = (alpha[t][i]*A[(i,j)]*B[(Obs[t+1],j)]*beta[t+1][j])/alphaSum
 
             #M-Step
@@ -206,24 +166,7 @@ def forwardBackward(AllObs, V, Q, Amat, Bmat, Pimat):
             for i in Q:
                 piHat[i] += gamma[1][i]
 
-        # print("________________")
-        # #print(A)
-        # Ast = A.copy()
-        # #normalize A
-
-
-        # #make up for eq1, 2
-        # for i in Q:
-        #     for j in Q:
-        #         aHat[(i,j)] = aHat[(i,j)] * obsProbSum
-        #     for sigma in V:
-        #         bHat[(sigma, i)] = bHat[(sigma, i)] * obsProbSum
-
-
-        # totstart = 0
-        # totend = 0
-        #
-
+        #normalize
         for i in Q:
             #get sum of state 1
             tot = 0
@@ -238,12 +181,13 @@ def forwardBackward(AllObs, V, Q, Amat, Bmat, Pimat):
         for i in Q:
             #get sum of state
             tot = 0
-            zeroB = 0
             for j in V:
                 tot += bHat[(j, i)]
             for j in V:
                 if tot != 0:
                  B[(j, i)] = bHat[(j, i)]/tot
+
+        #normalize pi
         tot = 0
         for i in Q:
             tot += piHat[i]
@@ -251,8 +195,14 @@ def forwardBackward(AllObs, V, Q, Amat, Bmat, Pimat):
             Pi[i] = piHat[i]/tot
 
 
-
-
+    #put pi into a, and log probabilities
+    for i in Q:
+        A[("START", i)] = Pi[i]
+    for i in Q:
+        for j in Q:
+            A[(i,j)] = log(A[(i,j)])
+        for j in V:
+            B[(j, i)] = log(B[(j, i)])
     return A, B
 
 
@@ -278,8 +228,9 @@ def main():
         del wds["<s>"]
     if "</s>" in wds:
         del wds["</s>"]
-    vocab = list(wds.keys())
 
+
+    vocab = list(wds.keys())
     dataSet = matrixes["allData"]
 
     vcb = {}
@@ -317,33 +268,6 @@ def main():
     Q = states
 
 
-    V = ["N", "E"]
-    Q = ["S1", "S2"]
-    Pi = Counter()
-    Pi["S1"] = 0.2
-    Pi["S2"] = 0.8
-    A = Counter()
-    B = Counter()
-    A[("S1", "S2")] = 0.5
-    A[("S1", "S1")] = 0.5
-    A[("S2", "S1")] = 0.3
-    A[("S2", "S2")] = 0.7
-
-    B[("N", "S1")] = 0.3
-    B[("N", "S2")] = 0.8
-    B[("E", "S1")] = 0.7
-    B[("E", "S2")] = 0.2
-
-    m = "NN,NN,NN,NN,NE,EE,EN,NN,NN"
-    b = m.split(",")
-    dataSet = []
-    for i in b:
-        sdd = []
-        for c in i:
-            if c == 'N' or c == 'E':
-                sdd.append(c)
-        dataSet.append(sdd)
-    print(dataSet)
     aHat, bHat = forwardBackward(dataSet, V, Q, A, B, Pi)
 
     print(aHat)
